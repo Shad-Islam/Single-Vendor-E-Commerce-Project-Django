e6dc5b8fbdd77e9d22cd3755925330f8fab1c9af
Shad-islam <m.shadislam01@gmail.com>
2025-05-20 22:49:43 +0600

    Add custom user model

--- E_Store/settings.py
+++ E_Store/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'accounts',
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
